05f2c41985722d83190dd1976536cc175edc2909
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-04-25 19:07:12 +0300

    Added a test to the dojo unit tests

--- Models/Tests/test_dojo.py
+++ Models/Tests/test_dojo.py
@@ -14,7 +14,12 @@ class TestDojo(unittest.TestCase):
 		the_dojo = Dojo({'location' : 'Nairobi', 'number_of_rooms' : 10})
 		self.assertIsInstance(the_dojo, Dojo, msg='The object should be an instance of the `Dojo` class')
 
+	#Test if the class creates a type of itself
+	def test_type(self):
+		the_dojo = Dojo({'location' : 'Nairobi', 'number_of_rooms' : 10})
+		self.assertTrue((type(the_dojo) is Dojo), msg='The object should be of type `Dojo`')
 
+	#Test if the class creates an accurate default model
 
 if __name__=="__main__":
 	unittest.main()
