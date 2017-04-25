8415c94fddec693c7d17a0f9f2eaddb107eeb48a
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-04-25 20:58:35 +0300

    Added a doc string to the dojo class and added test_room.py file to contain unit tests for the room class

---
+++ Models/Tests/test_room.py
@@ -0,0 +1,9 @@
+import unittest
+import sys
+
+sys.path.append('..')
+from room import room
+
+class TestRoom(unittest.TestCase):
+
+    """Test cases for the room class"""
--- Models/dojo.py
+++ Models/dojo.py
@@ -1,4 +1,5 @@
 class Dojo(object):
+    """The class describes the structure of the Dojo object"""
     def __init__(self, *arguments):
     	self.location = 'Nairobi'
     	if not not arguments:
@@ -8,6 +9,7 @@ class Dojo(object):
     		if len(arguments) > 2:
 		        self.occupied_rooms = arguments[2]
 
+    #This methods returns the number of vacant rooms in the Dojo object
     def get_vacant_rooms(self):
         result = 0
         if not not self.number_of_rooms: 
