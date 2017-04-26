e4a3d93ecf950e09f46e8d311483ff44d9691fab
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-04-26 14:11:22 +0300

    Added functionality for Person class

--- Models/Tests/test_person.py
+++ Models/Tests/test_person.py
@@ -20,7 +20,7 @@ class TestPerson(unittest.TestCase):
 
     # Test if the class creates a type of itself
     def test_type(self):
-        self.assertTrue((type(self.becky) is Person), msg='The object should be of type `Person`')
+        self.assertTrue((type(self.becky_staff) is Person), msg='The object should be of type `Person`')
 
     # Test if the class assigns the right attributes to person instances
     def test_firstname_attribute(self):
@@ -31,13 +31,13 @@ class TestPerson(unittest.TestCase):
         self.assertEqual('Soyer', self.tom_fellow.last_name, msg='The person last_name attribute should be `Soyer`')
 
     # Test if the class the right attribute type
-    def test_type(self):
+    def test_type_attribute(self):
         self.assertEqual(1, self.tom_fellow.type, msg='Tom should be a fellow')
         self.assertEqual(2, self.becky_staff.type, msg='Becky should be staff')
         self.assertEqual(2, self.reagan_staff_acc.type, msg='Reagan should be staff')
 
     # Test wants accomodation
-    def test_type(self):
+    def test_wants_accomodation(self):
         self.assertTrue(self.tom_fellow.has_living_space, msg='Tom should get accomodation')
         self.assertFalse(self.becky_staff.has_living_space, msg='Becky should not get accomodation')
         self.assertFalse(self.reagan_staff_acc.has_living_space, msg='Reagan should not get accomodation')
@@ -49,7 +49,7 @@ class TestPerson(unittest.TestCase):
         self.assertFalse(self.reagan_staff_acc.allocate_living_space(), msg='Reagan should not get accomodation')
 
     # Test if allocate_office method returns the right value for the given inputs
-    def allocate_office(self):
+    def test_allocate_office(self):
         self.assertTrue(self.tom_fellow.allocate_office(), msg='Tom should get an office')
         self.assertTrue(self.becky_staff.allocate_office(), msg='Becky should get an office')
         self.assertTrue(self.reagan_staff_acc.allocate_office(), msg='Reagan should get an office')
