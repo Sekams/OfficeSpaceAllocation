450fcbfc474003d8a5d11963d7950e7795657967
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-04-27 01:43:58 +0300

    Add more unittests for app

--- app/app.py
+++ app/app.py
@@ -8,16 +8,17 @@ from models.staff import Staff
 
 the_dojo = Dojo('Nairobi')
 
-def create_room(room_type, *room_name_arguments):
-	if room_name_arguments:
-		for room_name in room_name_arguments:
-			new_room = Room(room_type, room_name)
-			if new_room not in the_dojo.rooms:
-				the_dojo.rooms.append(new_room)
-				if new_room in the_dojo.rooms:
-					prefix = 'A'
-					if room_type.lower() == 'office':
-						prefix = 'An'
-					output = prefix + ' ' + room_type.lower() + ' called ' + room_name + ' has been successfully created!' 
-					print(output)
 
+def create_room(room_type, *room_name_arguments):
+    if room_name_arguments:
+        for room_name in room_name_arguments:
+            new_room = Room(room_type, room_name)
+            if new_room not in the_dojo.rooms:
+                the_dojo.rooms.append(new_room)
+                if new_room in the_dojo.rooms:
+                    prefix = 'A'
+                    if room_type.lower() == 'office':
+                        prefix = 'An'
+                    output = prefix + ' ' + room_type.lower() + ' called ' + room_name + 'has been successfully ' \
+                                                                                         'created! '
+                    # print(output)
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,17 @@ class TestApp(unittest.TestCase):
 
     # Test if the create_room function creates rooms when given input
     def test_create_room(self):
+        room_count_before = len(app.the_dojo.rooms)
         app.create_room('office', 'The Think Tank')
         app.create_room('livingspace', 'The Man Cave')
-        self.assertEqual(2, len(app.the_dojo.rooms),
+        room_count_after = len(app.the_dojo.rooms)
+        self.assertEqual(2, room_count_after - room_count_before,
+                         msg='The function should create 2 rooms in the `the-dojo` instance')
+
+    # Test if the create_room method function creates multiple rooms in one call
+    def test_create_multiple_rooms(self):
+        room_count_before = len(app.the_dojo.rooms)
+        app.create_room('office', 'Round', 'Square')
+        room_count_after = len(app.the_dojo.rooms)
+        self.assertEqual(2, room_count_after - room_count_before,
                          msg='The function should create 2 rooms in the `the-dojo` instance')
