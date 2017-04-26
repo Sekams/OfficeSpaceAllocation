a77958a634c31a1c6820fa2b68a701f6bf7ef837
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-04-26 11:16:56 +0300

    Added the room class, its unit tests and modified the dojo class and its unit tests to include a list of rooms

--- Models/Tests/test_dojo.py
+++ Models/Tests/test_dojo.py
@@ -3,21 +3,27 @@ import sys
 
 sys.path.append('..')
 from dojo import Dojo
+from room import Room
 
 
 class TestDojo(unittest.TestCase):
     
     """Test cases for the dojo class"""
+
+    # Instantiating objects for using in the tests
+    def setUp(self):
+        self.blue_office = Room('Office', 'Blue')
+        self.pink_living_space = Room('Living Space', 'Pink')
+        self.rooms = [self.blue_office, self.pink_living_space]
+        self.the_dojo = Dojo('Nairobi', self.rooms)
     
     # Test if the class creates an instance of itself
     def test_dojo_instance(self):
-        the_dojo = Dojo('Nairobi', 10)
-        self.assertIsInstance(the_dojo, Dojo, msg='The object should be an instance of the `Dojo` class')
+        self.assertIsInstance(self.the_dojo, Dojo, msg='The object should be an instance of the `Dojo` class')
 
     # Test if the class creates a type of itself
     def test_type(self):
-        the_dojo = Dojo('Nairobi', 10)
-        self.assertTrue((type(the_dojo) is Dojo), msg='The object should be of type `Dojo`')
+        self.assertTrue((type(self.the_dojo) is Dojo), msg='The object should be of type `Dojo`')
 
     # Test if the class creates an accurate default model
     def test_default_model_for_dojo(self):
@@ -26,15 +32,14 @@ class TestDojo(unittest.TestCase):
 
     # Test if the properties of the class are arranged in the proper order
     def test_dojo_properties(self):
-        kampala = Dojo('Kampala', 20)
-        self.assertListEqual(['Kampala', 20], [kampala.location, kampala.number_of_rooms],
-                             msg='The location and number_of_rooms of the Dojo should be properties arranged '
+        kampala = Dojo('Kampala', self.rooms)
+        self.assertListEqual(['Kampala', 2], [kampala.location, len(kampala.rooms)],
+                             msg='The location and rooms of the Dojo should be properties arranged '
                                  'respectively')
 
     # Test if the get_vacant_rooms method returns the right value for the given inputs
     def test_vacant_rooms(self):
-    	a_dojo = Dojo('Nairobi', 20, ['Room 1', 'Room 4'])
-    	self.assertEqual(18, a_dojo.get_vacant_rooms(), msg='The class has to return 18')
+    	self.assertEqual(2, len(self.the_dojo.get_vacant_rooms()), msg='The class has to return a list of 2 rooms')
 
 
 if __name__ == "__main__":
--- Models/dojo.py
+++ Models/dojo.py
@@ -1,17 +1,21 @@
 class Dojo(object):
     """The class describes the structure of the Dojo object"""
-    def __init__(self, *arguments):
-    	self.location = 'Nairobi'
-    	if not not arguments:
-    		self.location = arguments[0]
-    		if len(arguments) > 1:
-		        self.number_of_rooms = arguments[1]
-    		if len(arguments) > 2:
-		        self.occupied_rooms = arguments[2]
 
-    #This methods returns the number of vacant rooms in the Dojo object
+    def __init__(self, *arguments):
+        location = 'Nairobi'
+        rooms = []
+        if not not arguments:
+            location = arguments[0]
+            if len(arguments) > 1:
+                rooms.extend(arguments[1])
+        self.location = location
+        self.rooms = rooms
+
+    # This method returns a list of vacant rooms in the Dojo object
     def get_vacant_rooms(self):
-        result = 0
-        if not not self.number_of_rooms: 
-            result = self.number_of_rooms - len(self.occupied_rooms)
+        result = []
+        if not not self.rooms:
+            for a_room in self.rooms:
+                if a_room.capacity > a_room.occupied_spaces:
+                    result.append(a_room)
         return result
